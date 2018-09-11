8f56aa13a24c0c3fa781e03786b24043a12c0cdf
Kristofer Selbekk <selbeezy@gmail.com>
2018-09-11 12:19:11 +0200

    Add colors and fonts

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const fonts = {
+  serif: 'Newzald, Georgia, serif',
+  sansSerif: '"DIN OT", Calibri, serif',
+};
+
+export const colors = {
+  white: '#fff',
+  greyLighter: '#CECECE',
+  greyLight: '#BCBEC0',
+  greyMedium: '#8A8C8E',
+  greyDark: '#6D6E71',
+  greyDarker: '#464547',
+  black: '#0e0e0e',
+
+  red: '#ff5b5b',
+  orange: '#FF8034',
+  purple: '#8E24C9',
+  green: '#16DBC4',
+  darkBlue: '#162365',
+  greyBlue: '#7E9CB9',
+  lightBlue: '#43CBFF',
+};
